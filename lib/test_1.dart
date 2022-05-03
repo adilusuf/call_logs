af9dad704105c73e1ae7100a2d21af83d4e0c64c
adilusuf <adilhalidov1108@gmail.com>
2022-05-03 10:55:18 +0300

    master

--- lib/test_1.dart
+++ lib/test_1.dart
@@ -1,42 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(proverca());
+  runApp(sound_stream());
 }
-
-class proverca extends StatelessWidget {
-  const proverca({Key? key}) : super(key: key);
+class sound_stream extends StatelessWidget {
+  const sound_stream ({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        home: Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        leading:  IconButton(
-          icon : Icon(Icons.arrow_back,),color: Colors.white,
-          onPressed: (() {
-          }),
-        ),
-        title: Text('анифа шеф',style: TextStyle(color: Colors.white),),
-        actions: [ IconButton(
-          icon : Icon(Icons.settings),color: Colors.white,
-          onPressed: (() {
-          }),
-        ),
-       ],
-       
-      ),
-      body: Column(children:
-      [Row(
-        children: [
-          Container(child: Text('я',style: TextStyle(color: Colors.indigo),),),
-        ],
-      )
+    return MaterialApp(home: Scaffold(appBar: AppBar(title: Text('Главная'),)),
       
-      ]),
-      ),
     );
   }
 }
