2220970b5179767a1a41ce6434dce3715d5b6d7f
adilusuf <adilhalidov1108@gmail.com>
2022-05-07 10:00:01 +0300

    master

--- lib/whatsApp.dart
+++ lib/whatsApp.dart
@@ -3,20 +3,46 @@ import 'package:flutter/material.dart';
 void main(List<String> args) {
   runApp(WhatsApp());
 }
+
 class WhatsApp extends StatelessWidget {
-  const WhatsApp({ Key? key }) : super(key: key);
+  const WhatsApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-      appBar: AppBar(backgroundColor: Color.fromARGB(255, 51, 49, 49),
-        title: Text('WhatsApp',style: TextStyle(color: Colors.grey),
+          appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 51, 49, 49),
+        title: Text(
+          'WhatsApp',
+          style: TextStyle(color: Colors.grey),
         ),
-        actions: [Icon(Icons.search),Icon(Icons.more_vert)],
-        )
-    ),
-    );
+        actions: [
+          IconButton(
+          icon: Icon(Icons.search),
+              onPressed: () => {print("Click on settings button")}),
+               PopupMenuButton(
+              icon: Icon(Icons.more_vert),
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  value: 1,
+                  child: Text("Новая группа"),
+               ),
+               PopupMenuItem(
+                  value: 2,
+                  child: Text("Новая рассылка"),
+               ),
+               PopupMenuItem(
+                  value: 2,
+                  child: Text("Связанные устройства"),
+               ),PopupMenuItem(
+                  value: 2,
+                  child: Text("Избранные сообшения"),),
+                  PopupMenuItem(
+                  value: 2,
+                  child: Text("Настройки"),)]
+      )]),
+    ));
   }
 }
