6a47730cc97325f502e3bb60a232b1bafb8453b0
adilusuf <adilhalidov1108@gmail.com>
2022-05-16 18:07:38 +0300

    master
    Signed-off-by: adilusuf <adilhalidov1108@gmail.com>

--- lib/recent_calls/call_info_screen.dart
+++ lib/recent_calls/call_info_screen.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class CallInfo extends StatelessWidget {
   const CallInfo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    String? flutter_su;
+    var response = http.get(Uri.parse('http://json.flutter.su/echo'));
+    response.then((value) {
+      flutter_su = value.body;
+      print(flutter_su);
+    });
     return Scaffold(
       body: Container(
         color: Colors.orange[300],
         alignment: Alignment.center,
-        child: const Text("адильюсуф"),
+        child: Text(flutter_su ?? 'подождите...'),
       ),
     );
   }
